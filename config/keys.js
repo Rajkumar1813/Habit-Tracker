8f7b9a832da38a6e0bd2e4425dd7d49d13854049
Rajkumar <114818066+Rajkumar1813@users.noreply.github.com>
2025-03-23 21:17:28 +0530

    keys.js

--- config/keys.js
+++ config/keys.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-const db='mongodb+srv://fileshare1813:LK8i73GCMe4rSd6@cluster0.zf341md.mongodb.net/';
+const db='mongodb+srv://ohiabhist:duFoybPfg3hbfJPY@ohiabhist.zjaxj.mongodb.net/?retryWrites=true&w=majority&appName=ohiabhist';
           // mongodb+srv://fileshare1813:nn1z8uDnOfaAafi8@cluster0.zf341md.mongodb.net/
 
 const connectionparams={
